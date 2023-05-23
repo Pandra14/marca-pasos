bd8f489f43e844f2311425294e2d79c4bc7044fd
Pandra14 <130453008+Pandra14@users.noreply.github.com>
2023-05-23 11:59:56 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showNumber(steps * 0.64)
+})
 let steps = 0
+steps = 0
 basic.showNumber(0)
 basic.forever(function () {
-    if (input.acceleration(Dimension.Strength) > 2350) {
+    if (input.acceleration(Dimension.X) > 1500) {
         steps += 1
+        basic.pause(100)
     }
     if (input.buttonIsPressed(Button.A)) {
         basic.showNumber(steps)
